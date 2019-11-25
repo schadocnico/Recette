2ef5a654f712f993b4895bcfe65b624dbf86425c
Schad Nicolas <nico_schadoc@hotmail.fr>
2019-11-25 15:18:17 +0100

    class Recette

---
+++ app/src/main/java/fr/engrenage/recettes/Recette.java
@@ -0,0 +1,31 @@
+package fr.engrenage.recettes;
+
+import java.util.List;
+
+class Recette {
+    private String nom, URI;
+    private List<String> ingrediants;
+
+    public Recette(String nom, String URI) {
+        this.nom = nom;
+        this.URI = URI;
+    }
+
+    public Recette(String nom, String URI, List<String> ingrediants) {
+        this.nom = nom;
+        this.URI = URI;
+        this.ingrediants = ingrediants;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public List<String> getIngrediants() {
+        return ingrediants;
+    }
+
+    public String getURI() {
+        return URI;
+    }
+}
